575b2493d4a2144022b5fe96c8926568d625fd8a
lucas <lucaspsaracaju@gmail.com>
2022-12-11 18:14:01 -0300

    primeiro cmmit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function calcular(tipo, valor){
+    if(tipo == "acao"){
+        if(valor == "C"){
+            document.getElementById("tela").value = ""
+        }
+        if(valor == "+" || valor == "-" || valor == "*" || valor == "/" || valor == "."){
+            document.getElementById("tela").value += valor
+        }
+        if(valor == "="){
+            var valor_campo = eval(document.getElementById("tela").value)
+            document.getElementById("tela").value = valor_campo;
+        }
+    }else{
+        if(tipo == "valor"){
+            document.getElementById("tela").value += valor;
+        }
+    }
+}
